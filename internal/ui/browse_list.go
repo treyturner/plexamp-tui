afce56864802bed59f4ffdedbd43e70156070014
Trey Turner <treyturner@users.noreply.github.com>
2026-06-28 02:31:32 -0500

    refactor: remove remaining ui package globals

--- internal/ui/browse_list.go
+++ internal/ui/browse_list.go
@@ -1,6 +1,9 @@
 package ui
 
-import "github.com/charmbracelet/bubbles/list"
+import (
+	"github.com/charmbracelet/bubbles/list"
+	"github.com/charmbracelet/lipgloss"
+)
 
 func newBrowseList(title string, items []list.Item, width, height int) list.Model {
 	delegate := list.NewDefaultDelegate()
@@ -10,11 +13,15 @@ func newBrowseList(title string, items []list.Item, width, height int) list.Mode
 	browseList.Title = title
 	browseList.SetShowFilter(true)
 	browseList.SetFilteringEnabled(true)
-	browseList.Styles.Title = titleStyle
-	browseList.Styles.PaginationStyle = paginationStyle
-	browseList.Styles.HelpStyle = helpStyle
+	applyBrowseListStyles(&browseList)
 	resizeBrowseList(&browseList, width, height)
 	return browseList
+}
+
+func applyBrowseListStyles(browseList *list.Model) {
+	browseList.Styles.Title = lipgloss.NewStyle().MarginLeft(2)
+	browseList.Styles.PaginationStyle = list.DefaultStyles().PaginationStyle.PaddingLeft(4)
+	browseList.Styles.HelpStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("240")).Margin(1, 0, 0, 2)
 }
 
 func resizeBrowseList(browseList *list.Model, width, height int) {
